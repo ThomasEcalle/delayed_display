f1049a6b4756cb838d6463a2d3d026ec7d0f756e
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-11-06 01:46:50 +0100

    Finalize simple example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,40 +16,72 @@ class MyApp extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Duration initialDelay = Duration(seconds: 1);
+
     return Scaffold(
+      backgroundColor: Colors.amber,
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               DelayedDisplay(
+                delay: initialDelay,
                 child: Text(
-                  "OOOK",
+                  "Hello",
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
                     fontWeight: FontWeight.bold,
+                    fontSize: 35.0,
+                    color: Colors.white,
                   ),
                 ),
               ),
               DelayedDisplay(
+                delay: Duration(seconds: initialDelay.inSeconds + 1),
+                child: Text(
+                  "World",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              DelayedDisplay(
+                delay: Duration(seconds: initialDelay.inSeconds + 2),
                 child: Container(
                   width: 200,
-                  height: 40,
+                  height: 50,
                   decoration: BoxDecoration(
-                      color: Colors.blueAccent, borderRadius: BorderRadius.circular(20)),
+                      color: Colors.blueAccent, borderRadius: BorderRadius.circular(30)),
                   child: Center(
                     child: Text(
-                      "Click Here",
+                      "Subscribe",
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 18,
+                        fontSize: 24,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
-                delay: Duration(seconds: 1),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              DelayedDisplay(
+                delay: Duration(seconds: initialDelay.inSeconds + 3),
+                child: Text(
+                  "I already have an account",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ],
           ),
